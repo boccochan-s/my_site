9be2e6a585fe5256dcaab92fb92c6a7b9ba06986
boccochan_s <boccochan.s@gmail.com>
2026-01-01 11:07:55 +0900

    [add] css

--- ssg/src/render.rs
+++ ssg/src/render.rs
@@ -5,12 +5,15 @@ use crate::model::Post;
 pub fn render(posts: &[Post]) {
     let tera = Tera::new("templates/**/*").unwrap();
 
+    // CSS ファイルをコピー
+    fs::create_dir_all("dist/css").unwrap();
+    copy_css().unwrap_or_else(|e| eprintln!("CSS copy failed: {}", e));
+
     fs::create_dir_all("dist/posts").unwrap();
 
     for post in posts {
         let mut ctx = Context::new();
-        ctx.insert("title", &post.title);
-        ctx.insert("content", &post.content_html);
+        ctx.insert("post", &post);
 
         let html = tera.render("post.html", &ctx).unwrap();
         fs::write(
@@ -23,4 +26,18 @@ pub fn render(posts: &[Post]) {
     ctx.insert("posts", posts);
     let index_html = tera.render("index.html", &ctx).unwrap();
     fs::write("dist/index.html", index_html).unwrap();
+}
+
+fn copy_css() -> std::io::Result<()> {
+    if let Ok(entries) = fs::read_dir("templates/css") {
+        for entry in entries {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_file() {
+                let file_name = path.file_name().unwrap();
+                fs::copy(&path, format!("dist/css/{}", file_name.to_string_lossy()))?;
+            }
+        }
+    }
+    Ok(())
 }
